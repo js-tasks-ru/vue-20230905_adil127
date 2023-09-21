83e46ca4a0c751b28e07d3c80cb7c35218b20349
SAMask127node <104855433+SAMask127node@users.noreply.github.com>
2023-09-22 00:48:05 +0600

    3 tasks of HW-Vue basics completed

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,43 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import {createApp, defineComponent} from "./vendor/vue.esm-browser.js";
 
-// Создайте Vue приложение
+const App = defineComponent({
+  name: 'App',
+  // enum
+  OperationType: Object.freeze({
+    SUM: "sum",
+    SUBSTRACT: "subtract",
+    MULTIPLY: "multiply",
+    DIVIDE: "divide"
+  }),
+  data() {
+    return{
+      leftOperand: 0,
+      rightOperand: 0,
+      operationPicked: this.$options.OperationType.SUM,
+    }
+  },
+  computed: {
+    calculatedValue() {
+      const OperationType = this.$options.OperationType;
+      switch (this.operationPicked) {
+        case OperationType.SUM: {
+          return this.leftOperand + this.rightOperand;
+        }
+        case OperationType.SUBSTRACT: {
+          return this.leftOperand - this.rightOperand;
+        }
+        case OperationType.MULTIPLY: {
+          return this.leftOperand * this.rightOperand;
+        }
+        case OperationType.DIVIDE: {
+          return this.leftOperand / this.rightOperand;
+        }
+        default: {
+          alert("case default");
+        }
+      }
+    }
+  },
+});
+
+createApp(App).mount('#app');
